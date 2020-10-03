69db819d31c1c064a8a473a489bf55a7710b2937
Kara LeSueur <kara.lesueur@gmail.com>
2020-10-03 16:12:33 -0700

    styling on register toggle

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -17,31 +17,51 @@ function Register(props) {
     return (
         <div>
             <h1> register </h1>
-            <div>
-                <input placeholder={'First Name'} value={first} 
-                onChange={(e) => {setFirst(e.target.value)}} />
-                <input placeholder={'Last Name'} value={last} 
-                onChange={(e) => {setLast(e.target.value)}} />
-                <input placeholder={'Company'} value={company} 
-                onChange={(e) => {setCompany(e.target.value)}} />
-            </div>
-            <div>
-                <input placeholder={'Email'} value={email} 
-                onChange={(e) => {setEmail(e.target.value)}} />
-                <input placeholder={'Password'} type='password' value={password} 
-                onChange={(e) => {setPassword(e.target.value)}} />
-            </div>
-            <div>
-                <button onClick={ () => {
-                    axios.post('/auth/register', { email, first, last, company, password }).then((res) => {
-                        props.setUser(res.data)
-                        history.push('/Dashboard')                   
-                    }).catch((err) => {console.log(err)})
-                    //toast in ref code in past project
 
-                } }> submit! </button>
-                {/* //toast about email already being taken */}
+            <div>
+                <div>
+                    <input className='register-first'
+                    placeholder={'first name'} value={first} 
+                    onChange={(e) => {setFirst(e.target.value)}} />
+                    <input className='register-last'
+                    placeholder={'last name'} value={last} 
+                    onChange={(e) => {setLast(e.target.value)}} />
+                </div>
+                    
                 
+                <div>
+                    <input className='register-email'
+                    placeholder={'email'} value={email} 
+                    onChange={(e) => {setEmail(e.target.value)}} />
+                    <input className='register-company'
+                    placeholder={'company'} value={company} 
+                    onChange={(e) => {setCompany(e.target.value)}} />
+                </div>
+
+                <div>
+                    <input className='register-password'
+                    placeholder={'password'} type='password' value={password} 
+                    onChange={(e) => {setPassword(e.target.value)}} />
+                
+                    {/* <div> */}
+                        <button className='submit-button'
+                        onClick={ () => {
+                            axios.post('/auth/register', { email, first, last, company, password }).then((res) => {
+                                props.setUser(res.data)
+                                history.push('/Dashboard')                   
+                            }).catch((err) => {console.log(err)})
+                            //toast in ref code in past project
+
+                        } }> s u b m i t </button>
+                        {/* //toast about email already being taken */}
+
+                        <button 
+                        //button is broken
+                        className='reg-register-button' 
+                        onClick={ () => {props.isRegistered(false)}}>back to login</button>
+
+                    {/* </div> */}
+                </div>  
             </div>
         </div>
     )
