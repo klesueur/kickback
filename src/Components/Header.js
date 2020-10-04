0ab6cd216363afd9d4e3f65a98d49eff37b0c3c5
Kara LeSueur <kara.lesueur@gmail.com>
2020-10-03 18:39:41 -0700

    dash styling started

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,15 +9,19 @@ function Header(props) {
 
     return (
         <div className='header'>
-            header
+
+            <div className='logo'>
+                //logo here
+            </div>
             
             <div>
+                <p className='dash-kickback'>kickback</p>
                 <button onClick={() => {props.history.push('/dashboard')}}> Dashboard </button>
                 <button onClick={() => {props.history.push('/merchstore')}}> Merch Store </button>
             </div>
 
             <div>
-                <h1> Hello, {props.user && props.user.first}</h1>
+                <p className='greeting'> Hello, {props.user && props.user.first}</p>
                 <button onClick={ () => {
                     props.logoutUser() 
                     props.history.push('/')
