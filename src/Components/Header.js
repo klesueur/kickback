6374777a3e3cabf53d6c8ec9ced9b4c124e33ab8
Kara LeSueur <kara.lesueur@gmail.com>
2020-10-03 18:57:17 -0700

    more header styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,20 +14,23 @@ function Header(props) {
                 //logo here
             </div>
             
-            <div>
-                <p className='dash-kickback'>kickback</p>
-                <button onClick={() => {props.history.push('/dashboard')}}> Dashboard </button>
-                <button onClick={() => {props.history.push('/merchstore')}}> Merch Store </button>
+            <div className='header-content'>
+                <div className='kickback-and-nav'>
+                    <div>
+                        <p className='dash-kickback'>kickback</p>
+                        <button onClick={() => {props.history.push('/dashboard')}}> Dashboard </button>
+                        <button onClick={() => {props.history.push('/merchstore')}}> Merch Store </button>
+                    </div>
+                </div>
+          
+                <div className='greeting-and-logout'>
+                    <p className='greeting'> Hello, {props.user && props.user.first}</p>
+                    <button onClick={ () => { 
+                        props.logoutUser()
+                        props.history.push('/')
+                    }}> Logout </button>
+                </div>
             </div>
-
-            <div>
-                <p className='greeting'> Hello, {props.user && props.user.first}</p>
-                <button onClick={ () => {
-                    props.logoutUser() 
-                    props.history.push('/')
-                  }}> Logout </button>
-            </div>
-
         </div>
     )
 }
