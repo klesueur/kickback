e90d8a341d4bc796bf1e334034c709f14cc36a7b
Kara LeSueur <kara.lesueur@gmail.com>
2020-10-07 14:09:05 -0700

    cart

--- src/Components/MerchShop/MerchItem.js
+++ src/Components/MerchShop/MerchItem.js
@@ -14,12 +14,14 @@ function MerchItem(props) {
                 <img className='product-img' src={props.data.img} 
                 width='190' height='175' />
         
+                <div className='text-below'>
+                    <p className='product-name'> {props.data.name}</p>
+                    <p className='product-price'> ${props.data.price} </p>
 
-                <p className='product-name'> {props.data.name}</p>
-                <p className='product-price'> ${props.data.price} </p>
-
-
-
+                    <button className='add-to-cart-button'>
+                        add
+                    </button>
+                </div>
             </div>
         )
     
